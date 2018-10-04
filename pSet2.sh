9ff30bc975c9d1988324301e35edf822a56cb08b
Juan Guerra <juan@Juans-MBP.wireless.yale.internal>
2018-10-04 11:20:16 -0400

    Initial commit: Script for Stellar Astrophysics Fall 18 problem set 2. It probably doesnt work completely right at the moment, gotta ask Joel to check it over before running it.

---
+++ pSet2.sh
@@ -0,0 +1,71 @@
+#!bin/bash
+
+#title           :stellarScript.sh
+#description     :Stellar Astrophysics Problem set 2 - P7,P8
+#author          :Juan Guerra
+#date            : 08/11/18
+#usage           :bash mkscript.sh
+#==============================================================================
+
+
+# Added mixing_length_alpha=2 to inlist_project							-- Needed for problem 8
+# set stopping parameter by commenting: stop_near_zams = .true. and
+# uncommented: xa_central_lower_limit(1) = 4.5d-1  						-- Don't know if this needs to be changed for Problem 8
+
+# make sure to be in /scratch/juan, and that scripts.sh is in same directory 
+# import scripts.sh so that i can use change_param() <-ask joel if I need the other functions for some reason 
+
+. ./scripts.sh
+./mk
+
+# Problem 7:
+# The mass luminosity relation of stars using MESA: Make models of mass 1 to 5 Msun in intervals of 0.2 Msun. 
+# Keep the initial abundances and mixing length parameters and other physics the same for all of them. At the 
+# point where the central central hydrogen mass fraction is 0.45, examine whether the relation L ∝ M^3 does indeed hold. 
+# The central hydrogen mass fraction is printed out in the history file.
+
+
+# I set xa_central_lower_limit(1) = 4.5d-1 so that I only have to look at the last points of L for each run. Then I'll plot L vs M to see if  L ∝ M^3 
+M_0=1.0
+step='.2'   # addding decimals in bash scripts is apparently annoying to do -- but i found the following way of doing it online.
+
+
+for i in {0..20}
+do
+	change_param initial_mass "$(echo "$M_0 + $step * $i " | bc)" inlist_project
+	./rn
+	mv LOGS LOGS_"$(echo "$start + $M_0 * $i " | bc)"
+done
+
+# Then I'll plot L vs M to see if  L ∝ M^3 
+
+
+#Problem 8:
+# The effect of the mixing length parameter: Change the mixing length parameter of the models by ±0.4 and remake the models, 
+# for this exercise a mass interval of 1Msun is sufficient. Plot the tracks for the three different mixing lengths. What is 
+# the most notable difference in terms of radius and temperature. You should keep the abundances the same.
+
+
+# NOTE:  Not sure what she means by "for the exercise a  mass interval of 1Msun is sufficient" 
+
+initialAlpha=1.6
+aplhaStep='.4'
+
+change_param initial_mass 1 inlist_project #Reset the mass to 1 solar mass 
+
+for j in {0,1,2}
+do 
+	change_param mixing_length_alpha $(echo "$initialAlpha + $aplhaStep * $i") inlist_project
+	./rn 
+	mv LOGS LOGS_"$(echo "$start + $M_0 * $i " | bc)"
+done
+}
+
+#QUESTIONS:
+
+#could I have set 
+
+
+
+
+
---
+++ scripts.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+# Useful building blocks on the shell for MESA
+# Some of these originally written by Earl Bellinger
+# (formerly grad student at Yale, now a postdoc at the SAC in Aarhus)
+# To use these, either source this file, or include these function
+# definitions at the beginning of the shell script from which you
+# are invoking MESA.
+
+change_param() { 
+	# Modifies a parameter in the current inlist. 
+	# args: ($1) name of parameter 
+	#       ($2) new value 
+	#       ($3) filename of inlist where change should occur 
+	# Additionally changes the 'inlist_0all' inlist. (you can comment this out)
+	# example command: change_param initial_mass 1.3 inlist_1pms
+	# example command: change_param log_directory 'LOGS_MS' inlist_project
+	# example command: change_param do_element_diffusion .true. inlist
+	param=$1 
+	newval=$2 
+	filename=$3 
+	search="^\s*\!*\s*$param\s*=.+$" 
+	replace="      $param = $newval" 
+	sed -r -i.bak -e "s/$search/$replace/g" $filename 
+	
+	if [ ! "$filename" == 'inlist_0all' ]; then 
+		change_param $1 $2 "inlist_0all" 
+	fi 
+} 
+
+set_inlist() { 
+	# Changes to a different inlist by modifying where "inlist" file points 
+	# args: ($1) filename of new inlist  
+	# example command: change_inlists inlist_2ms 
+	# By assumption, extra_star_job_inlist1_name (and controls) points
+	# to inlist_0all.
+	newinlist=$1 
+	echo "Changing to $newinlist" 
+	change_param "extra_star_job_inlist2_name" "'$newinlist'" "inlist" 
+	change_param "extra_controls_inlist2_name" "'$newinlist'" "inlist" 
+}
+
+restart_run() {
+	./re `/usr/bin/ls tracks/photos -t | head -n 1`
+}
